a28de5fe2b356683fff30d6dbc744e9ed4344f43
shivbh17 <shivbh@umich.edu>
2026-03-23 17:02:24 -0400

    Implement create_listing_database

--- project2_starter.py
+++ project2_starter.py
@@ -186,7 +186,24 @@ def create_listing_database(html_path) -> list[tuple]:
     # ==============================
     # YOUR CODE STARTS HERE
     # ==============================
-    pass
+    listings = load_listing_results(html_path)
+
+    database = []
+    for listing_title, listing_id in listings:
+        details = get_listing_details(listing_id)
+        info = details[listing_id]
+        row = (
+            listing_title,
+            listing_id,
+            info["policy_number"],
+            info["host_type"],
+            info["host_name"],
+            info["room_type"],
+            info["location_rating"]
+        )
+        database.append(row)
+
+    return database
     # ==============================
     # YOUR CODE ENDS HERE
     # ==============================
